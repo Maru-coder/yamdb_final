afd369a43d8f52049d04911a2a49f020fb6b89e3
Valerij Ezhov <ezhov.fzzz@gmail.com>
2023-04-20 01:31:04 +0600

    isort_flake

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,16 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (
-    CategoryViewSet,
-    CommentViewSet,
-    GenreViewSet,
-    ReviewViewSet,
-    signup,
-    TitleViewSet,
-    token,
-    UserViewSet,
-)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet, signup, token)
+
 app_name = 'api'
 
 router = routers.DefaultRouter()
